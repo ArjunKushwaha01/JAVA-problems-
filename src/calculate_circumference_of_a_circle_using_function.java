1b037771d0b3ddd6d73176f9a7107fd84d725c94
ArjunKushwaha01 <kushwahaarjun920@gmail.com>
2024-03-15 21:37:19 +0530

    eligible to vote

--- src/calculate_circumference_of_a_circle_using_function.java
+++ src/calculate_circumference_of_a_circle_using_function.java
@@ -2,11 +2,13 @@ import java.util.Scanner;
 
 public class calculate_circumference_of_a_circle_using_function {
     public static void CircumferenceOfaCircle(int r){
+        System.out.println("the circumference of a circle is :");
         System.out.println((int) (2*3.14*r));
         return;
     }
     public static void main(String[] args){
         Scanner sc= new Scanner(System.in);
+        System.out.println("enter the radius to find circumference of a circle :");
         int a = sc.nextInt();
         CircumferenceOfaCircle(a);
     }
