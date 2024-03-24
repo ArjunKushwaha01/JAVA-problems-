8110fb1304e14cdfeb9ae781a66ef55f7264f4c4
ArjunKushwaha01 <kushwahaarjun920@gmail.com>
2024-03-24 11:53:44 +0530

    infinite loop

--- src/input_2_num_return_greater_num.java
+++ src/input_2_num_return_greater_num.java
@@ -3,12 +3,18 @@ import java.util.Scanner;
 
 public class input_2_num_return_greater_num {
     public static void greaternum(int a,int b){
-        int n [] = {a,b};
-        System.out.println(Arrays.stream(n).max());
+        System.out.println("the greater value is :");
+        if (a > b){
+            System.out.println(a);
+        }else {
+            System.out.println(b);
+        }
     }
     public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
+        System.out.println("enter the first value");
         int a = sc.nextInt();
+        System.out.println("enter the second value");
         int b = sc.nextInt();
         greaternum(a,b);
     }
