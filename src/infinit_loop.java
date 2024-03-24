6148b75174ddbc9a7108fd3eee02c4090c893e2d
ArjunKushwaha01 <kushwahaarjun920@gmail.com>
2024-03-24 11:55:36 +0530

    infinite loop

--- src/infinit_loop.java
+++ src/infinit_loop.java
@@ -2,9 +2,8 @@ public class infinit_loop {
     public static void main(String [] args){
         int n = 1;
         do {
-            for (int i =1;i<=i;i++)
-            System.out.println(n);
-              n++;
-        }while (n<=0);
+
+
+        }while (true);
     }
 }
